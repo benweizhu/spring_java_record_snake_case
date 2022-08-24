18e5859e7d03a0e6033fc55bbb583fd25f5e4cf2
benwei.zhu <benwei.zhu@hyrecar.com>
2022-08-24 16:48:48 +0800

    convert Map to camelCase

--- src/main/java/com/example/snake_case/UserController.java
+++ src/main/java/com/example/snake_case/UserController.java
@@ -1,9 +1,19 @@
 package com.example.snake_case;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class UserController {
+
+    private final ObjectMapper objectMapper;
+
+    public UserController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @PostMapping(value = "/users")
     public User createUser(@RequestBody User user) {
         return user;
@@ -13,4 +23,13 @@ public class UserController {
     public User getUser(@PathVariable long id) {
         return new User("firstName", "lastName", new Organization("firstName"));
     }
+
+    @GetMapping(value = "/users")
+    public User getUser(@RequestParam Map<String, String> map) {
+        Object object = objectMapper.convertValue(map, Object.class);
+        Map<String, String> newMap = objectMapper.convertValue(object, Map.class);
+
+        return new User(newMap.get("firstName"), "lastName", new Organization("firstName"));
+    }
+
 }
